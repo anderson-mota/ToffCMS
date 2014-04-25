7324eef48cfc3433396d14f603ce414c96a6e09f
Matiss Janis Aboltins <matiss.ja@gmail.com>
2014-04-25 15:22:49 +0300

    Another dependency fix

--- app/scripts/controllers/page.js
+++ app/scripts/controllers/page.js
@@ -4,6 +4,10 @@ app.controller('PageCtrl', function ($scope, $routeParams, Page) {
     $scope.pages = [];
     $scope.activeStatus = 'all';
 
+    $scope.do = function ($event) {
+      $event.preventDefault();
+    };
+
     $scope.showByStatus = function (status) {
       for (var i = 0; i < $scope.pages.length; i++) {
         $scope.pages[i].isVisible = ($scope.pages[i].status === status || status === 'all');
@@ -39,6 +43,29 @@ app.controller('PageCtrl', function ($scope, $routeParams, Page) {
 
       // ToDo: delete the page server-side
     };
+
+
+    // Make the editor look smaller
+    $.fn.wysihtml5.locale.en = jQuery.extend($.fn.wysihtml5.locale.en, {
+      emphasis: {
+        bold: 'B',
+        italic: 'I',
+        underline: 'U',
+        small: 'S'
+      }
+    });
+
+    // Init wysiwyg
+    $(".textarea").wysihtml5({
+      toolbar: {
+        color: true,
+        html: true
+      }
+    });
+
+    $('input[name="title"]').keyup(function () {
+      $('input[name="slug"]').val( slug($(this).val()) );
+    });
 
     // Get all of the pages
     Page.get(function (data) {
