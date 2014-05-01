55db879663a4e556291cc82286b9d8471b32a821
Matiss Janis Aboltins <matiss.ja@gmail.com>
2014-05-01 13:20:06 +0300

    Sidebar toggle button fixed to actually work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -71,4 +71,23 @@ var app = angular.module('adminApp', [
       $event.preventDefault();
     };
 
+    /**
+     * Hide the sidebar - on/off canvas toggle
+     */
+    $rootScope.offCanvas = function (event) {
+      event.preventDefault();
+
+      // If window is small enough, enable sidebar push menu
+      if ($(window).width() <= 992) {
+        $('.row-offcanvas').toggleClass('active');
+        $('.left-side').removeClass("collapse-left");
+        $(".right-side").removeClass("strech");
+        $('.row-offcanvas').toggleClass("relative");
+      } else {
+        // Else, enable content streching
+        $('.left-side').toggleClass("collapse-left");
+        $(".right-side").toggleClass("strech");
+      }
+    };
+
   });
