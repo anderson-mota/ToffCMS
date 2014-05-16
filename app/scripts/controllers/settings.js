3c62732044a9e077537026cf2defeb7b611b5803
Matiss Janis Aboltins <matiss.ja@gmail.com>
2014-05-16 11:52:23 +0300

    Settings can now be saved

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -1,9 +1,30 @@
 'use strict';
 
-app.controller('SettingsCtrl', function ($scope, Settings, LANGUAGES) {
+app.controller('SettingsCtrl', function ($scope, Settings) {
 
     $scope.settings = [];
-    $scope.languages = LANGUAGES;
+    $scope.formErrors = [];
+    $scope.processingForm = false;
+
+    /**
+     * Process the current form
+     * @return {void}
+     */
+    $scope.processForm = function () {
+      $scope.processingForm = true;
+
+      // Error processing
+      var error = function (data) {
+        console.log(data);
+        $scope.formErrors = data.data.message;
+        $scope.processingForm = false;
+      };
+
+      // Update the settings
+      Settings.update({ settings: $scope.settings }, function success () {
+        $scope.processingForm = false;
+      }, error);
+    };
 
     Settings.get(function (data) {
       $scope.settings = data.settings;
